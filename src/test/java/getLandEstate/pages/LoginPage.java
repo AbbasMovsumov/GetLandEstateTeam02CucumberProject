f0defe8cb3d5b3fbf7826c5075fed8074a529488
MurvetAyyildiz <murvetayyildiz@gmail.com>
2024-07-11 14:40:52 +0200

    logineklendi

--- src/test/java/getLandEstate/pages/LoginPage.java
+++ src/test/java/getLandEstate/pages/LoginPage.java
@@ -1,4 +1,39 @@
 package getLandEstate.pages;
 
+import getLandEstate.utilities.ConfigReader;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class LoginPage {
+
+    @FindBy(xpath = "")
+    public WebElement username;
+    @FindBy(xpath = "")
+    public WebElement password;
+    @FindBy(xpath = "")
+    public WebElement loginButton;
+
+    public void loginWithRole(String role) {
+
+        switch (role) {
+            case "Admin":
+                username.sendKeys(ConfigReader.getProperty("adminEmail"));
+                password.sendKeys(ConfigReader.getProperty("adminPassword"));
+                loginButton.click();
+                break;
+            case "Manager":
+                username.sendKeys(ConfigReader.getProperty("managerEmail"));
+                password.sendKeys(ConfigReader.getProperty("managerPassword"));
+                loginButton.click();
+                break;
+            case "Customer":
+                username.sendKeys(ConfigReader.getProperty("customerEmail"));
+                password.sendKeys(ConfigReader.getProperty("customerPassword"));
+                loginButton.click();
+                break;
+            default:
+                throw new IllegalArgumentException(String.format("Unexpected value for role %s", role));
+        }
+    }
 }
--- src/test/java/getLandEstate/stepDefinitons/Test.java
+++ src/test/java/getLandEstate/stepDefinitons/Test.java
@@ -1,4 +1,14 @@
 package getLandEstate.stepDefinitons;
 
+import getLandEstate.pages.LoginPage;
+import getLandEstate.pages.Pages;
+import io.cucumber.java.en.Given;
+
 public class Test {
+    Pages pages = new Pages();
+    @Given("user login as {string}")
+    public void user_login_as(String role) {
+        pages.loginPage().loginWithRole("Admin");
+
+    }
 }
