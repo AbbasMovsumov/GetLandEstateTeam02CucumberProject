b4e77d22508978a936d2e26d599319ec2ad5b515
burhanettin <burhanettin>
2024-07-16 23:29:20 +0200

    V1

--- src/test/java/getLandEstate/pages/HomePage.java
+++ src/test/java/getLandEstate/pages/HomePage.java
@@ -1,4 +1,42 @@
 package getLandEstate.pages;
 
-public class HomePage {
+import getLandEstate.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePageSale {
+
+    public HomePageSale(){PageFactory.initElements(Driver.getDriver(), this); }
+
+    @FindBy(xpath = "//*[@name=`query']")
+    public WebElement searchButton;
+
+    @FindBy(xpath = "//*[@class=`btn btn-primary']")
+    public WebElement searchItem;
+
+    @FindBy(xpath = "//*[@id=`ps']")
+    public WebElement minimumValue ;
+
+    @FindBy(xpath = "//*[@id=`pe']")
+    public WebElement maximumValue ;
+
+    @FindBy(xpath = "//*[@id=`at']")
+    public WebElement advertTypeButton;
+
+    @FindBy(xpath = "//*[@id=`c']")
+    public WebElement categoryButton;
+
+    @FindBy(xpath = "//*[@id='ctry']")
+    public WebElement countryButton;
+
+    @FindBy(xpath = "//*[@id=`city']")
+    public WebElement cityButton;
+
+    @FindBy(xpath = "//*[@class=`dynamic-button  btn btn-primary']")
+    public WebElement searchButton2;
+
+    @FindBy(xpath = "//*[@class=`property-card card']")
+    public WebElement pictureBox;
 }
+
--- src/test/java/getLandEstate/stepDefinitons/uiStepDef/HomeStepDef.java
+++ src/test/java/getLandEstate/stepDefinitons/uiStepDef/HomeStepDef.java
@@ -1,20 +1,22 @@
 package getLandEstate.stepDefinitons.uiStepDef;
 
-import getLandEstate.pages.HomePage;
-import getLandEstate.utilities.ConfigReader;
+import getLandEstate.pages.HomePageSale;
 import getLandEstate.utilities.Driver;
 import getLandEstate.utilities.ReusableMethods;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.WebElement;
 
-import static getLandEstate.utilities.ReusableMethods.click;
+public class HomeStepDefSale extends ReusableMethods {
+    HomePageSale homePageSale=new HomePageSale();
+    @Given("user is on the google page")
+    public void user_is_on_the_google_page() {
+        Driver.getDriver().get("https://www.getlandestate.com");
+    }
 
-public class HomeStepDef extends ReusableMethods {
-    HomePage homePage=new HomePage();
     @Given("Go to the website")
     public void go_to_the_website() {
-        Driver.getDriver().get(ConfigReader.getProperty("url"));
+
     }
     @When("Click on the search box")
     public void click_on_the() {
@@ -24,4 +26,115 @@ public class HomeStepDef extends ReusableMethods {
     public void type_into_the_search(String property) {
        // homePage.searchBox.sendKeys(property);
     }
+
+    @When("user navigates to the home page")
+    public void user_navigates_to_the_home_page() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @When("user verifies to the homepage")
+    public void user_verifies_to_the_homepage() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @Then("user clicks search item")
+    public void user_clicks_search_item() {
+        // Write code here that turns the phrase above into concrete actions// throw new io.cucumber.java.PendingException();
+    }
+    @Then("user enters {string}")
+    public void user_enters(String string) {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @Then("user clicks search button")
+    public void user_clicks_search_button() {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @Then("user enters Price Range {string} and {string}")
+    public void user_enters_price_range_and(String price , String range) {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user cliks Advert Type button")
+    public void user_cliks_advert_type_button() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user choeses \"<Sale>\"Advert Type")
+    public void user_choeses_sale_advert_type() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user cliks Category button")
+    public void user_cliks_category_button() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user choeses \"<House>\" Category")
+    public void user_choeses_house_category() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user cliks Country button")
+    public void user_cliks_country_button() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user choeses \"<Türkiye>\"Country")
+    public void user_choeses_türkiye_country() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user cliks City button")
+    public void user_cliks_city_button() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user choeses\"<Antalya>\"City")
+    public void user_choeses_antalya_city() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user cliks District button")
+    public void user_cliks_district_button() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user choeses \"<Alanya>\" District")
+    public void user_choeses_alanya_district( ) {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user cliks a Search button2")
+    public void user_cliks_a_search_button2() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("user sees picture")
+    public void user_sees_picture() {
+        // Write code here that turns the phrase above into concrete actions
+    ;
+    }
+    @Then("user click on the Picture Box")
+    public void user_click_on_the_picture_box() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+   @Then("user cliks a Search button")
+    public void user_cliks_a_search_button() {
+            // Write code here that turns the phrase above into concrete actions
+
+        }
+    @Then("user clicks on the Picture")
+    public void user_click_on_the_picture() {
+            // Write code here that turns the phrase above into concrete actions
+
+        }
+    @Then("user sees Description and Location information")
+    public void user_sees_description_and_location_information() {
+            // Write code here that turns the phrase above into concrete actions
+
+    }
+
+
+
 }
